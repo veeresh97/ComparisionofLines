a33ce76d61c043b79bab5f17576ec38fa6acb83d
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-01 11:46:12 +0530

    Added: checking the lines equal or not

--- comparisionLines.java
+++ comparisionLines.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
 
 public class comparisionLines {
-	 public static void main(String[] args) {
-			Scanner myobj = new Scanner(System.in);
-			System.out.println("enter x1 and y1 values");
-			int x1 = myobj.nextInt();
-			int y1 = myobj.nextInt();
-			System.out.println("enter x2 and y2 values");
-	                int x2 = myobj.nextInt();
-	                int y2 = myobj.nextInt();
-			double length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-			System.out.println( length);
-	        }
+	public static void main(String[] args) {
+		Scanner myobj = new Scanner(System.in);
+		System.out.println("enter x1 and y1 values");
+		int x1 = myobj.nextInt();
+		int y1 = myobj.nextInt();
+		System.out.println("enter x2 and y2 values");
+                int x2 = myobj.nextInt();
+                int y2 = myobj.nextInt();
+		double length1 = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		System.out.println("length1: "+length1);
+		System.out.println("enter a1 and b1 values");
+                int a1 = myobj.nextInt();
+                int b1 = myobj.nextInt();
+                System.out.println("enter a2 and b2 values");
+                int a2 = myobj.nextInt();
+                int b2 = myobj.nextInt();
+		double length2 = Math.sqrt(Math.pow(a2-a1,2)+Math.pow(b2-b1,2));
+		System.out.println("length2: "+length2);
+		if ( length1==length2)
+			System.out.println("length is equal");
+		else
+			System.out.println("length is not equal");
+        }
 
 }
